27b1ebecc82a579add5dc124f0915c9f5e13f86d
cloudsforest <cloudsforest@gmail.com>
2019-05-08 12:23:41 +0100

    Update dialog.py

--- cellprofiler/gui/dialog.py
+++ cellprofiler/gui/dialog.py
@@ -3,8 +3,8 @@
 import raven
 import wx
 
-import wx.adv
-class AboutDialogInfo(wx.adv.AboutDialogInfo):
+
+class AboutDialogInfo(wx.AboutDialogInfo):
     def __init__(self):
         super(AboutDialogInfo, self).__init__()
 
